1b7f77bfd096d1fc1abd3917786d08dcfa14e045
Vincent <vincent.weber@etu.univ-amu.fr>
2019-05-03 16:17:14 +0100

    Partie chambre faite

---
+++ src/main/java/Test/ChambreTest.java
@@ -0,0 +1,56 @@
+package Test;
+
+import code.Chambre;
+import code.Hotel;
+import code.model.DAOJDBC.DAOChambreJDBC;
+import code.model.DAOJDBC.DAOHotelJDBC;
+import javafx.util.Pair;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Vincent on 03/05/2019.
+ */
+public class ChambreTest {
+
+    private DAOChambreJDBC daoChambreJDBC = new DAOChambreJDBC();
+
+    @Test
+    public void testInsertChambre() {
+
+        Chambre chambre = new Chambre();
+        chambre.setHotel(new DAOHotelJDBC().getById(1));
+        chambre.setNumChambre(213);
+        chambre.setEtat("OPEN");
+        chambre.setType("Standard");
+
+        Chambre chambreInsert = daoChambreJDBC.insert(chambre);
+        assertTrue(chambre == chambreInsert);
+    }
+
+    @Test
+    public void testUpdateChambre() {
+
+        Chambre chambre = daoChambreJDBC.getById(new Pair<>(1,213));
+        System.out.println(chambre);
+        chambre.setEtat("USED");
+        assertTrue(daoChambreJDBC.update(chambre));
+    }
+
+    @Test
+    public void testFindAllChambres() {
+        List<Chambre> chambres = daoChambreJDBC.FindAll();
+        assertTrue(chambres.size() == daoChambreJDBC.getNbChambres());
+        System.out.println(chambres);
+    }
+
+    @Test
+    public void testDeleteChambre() {
+        Chambre chambre = daoChambreJDBC.getById(new Pair<>(1,213));
+        assertTrue(daoChambreJDBC.delete(chambre));
+    }
+
+}
